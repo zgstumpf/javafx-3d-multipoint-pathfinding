c2e7a06dc8081bf8335616a0680849f6353c1f51
Zach Stumpf <90022855+zgstumpf@users.noreply.github.com>
2024-10-14 00:02:01 -0400

    camera controls

--- src/main/java/zstumpf/learn_javafx2/HelloApplication.java
+++ src/main/java/zstumpf/learn_javafx2/HelloApplication.java
@@ -1,22 +1,46 @@
 package zstumpf.learn_javafx2;
 
 import javafx.application.Application;
+import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.shape.Sphere;
 import javafx.stage.Stage;
 import javafx.scene.Group;
+import javafx.scene.Camera;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private final static double WIDTH = 1000;
+    private final static double HEIGHT = 800;
+
     @Override
     public void start(Stage stage) throws IOException {
         Sphere sphere = new Sphere(50);
+        sphere.translateXProperty().set(WIDTH/2);
+        sphere.translateYProperty().set(HEIGHT/2);
+
         Group group = new Group();
         group.getChildren().add(sphere);
 
+        stage.addEventHandler(KeyEvent.KEY_PRESSED, keyEvent -> {
+            switch (keyEvent.getCode()) {
+                case W:
+                    sphere.translateZProperty().set(sphere.getTranslateZ() - 50);
+                    break;
+                case S:
+                    sphere.translateZProperty().set(sphere.getTranslateZ() + 50);
+                    break;
+            }
+        });
+
+        Camera camera = new PerspectiveCamera();
+        Scene scene = new Scene(group, WIDTH, HEIGHT);
+        scene.setCamera(camera);
+
         stage.setTitle("JavaFX Stage");
-        stage.setScene(new Scene(group,1000,1000));
+        stage.setScene(scene);
         stage.show();
     }
 
