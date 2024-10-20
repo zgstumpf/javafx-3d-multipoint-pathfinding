c88179c0facc3e7469dfb535e8e5a83e1370a377
Zach Stumpf <90022855+zgstumpf@users.noreply.github.com>
2024-10-19 23:49:32 -0400

    bug fixes

--- src/main/java/zstumpf/learn_javafx2/Target.java
+++ src/main/java/zstumpf/learn_javafx2/Target.java
@@ -1,6 +1,7 @@
 package zstumpf.learn_javafx2;
 
 import javafx.geometry.Point2D;
+import javafx.scene.Node;
 import javafx.scene.SubScene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
@@ -24,11 +25,15 @@ public class Target extends Sphere {
     // The id, or identifier, is used to tell Targets apart.
     public final int id;
 
+    // The target's label that appears on the map.
+    public Label label;
+
     // The Point3D associated with the Target
     public Point3D point3D;
 
     public Target(int id, double translateX, double translateY, double translateZ){
         this.id = id;
+        this.label = new Label(String.valueOf(this.id));
 
         // Position
         this.setTranslateX(translateX);
@@ -57,9 +62,47 @@ public class Target extends Sphere {
      * @param map3D
      */
     public void renderId(boolean render, SubScene map3D, Camera camera, Pane labelPane) {
+        /**
+         * FloatingLabels.java
+         *
+         * Copyright (c) 2013-2017, F(X)yz
+         * All rights reserved.
+         *
+         * Redistribution and use in source and binary forms, with or without
+         * modification, are permitted provided that the following conditions are met:
+         *     * Redistributions of source code must retain the above copyright
+         * notice, this list of conditions and the following disclaimer.
+         *     * Redistributions in binary form must reproduce the above copyright
+         * notice, this list of conditions and the following disclaimer in the
+         * documentation and/or other materials provided with the distribution.
+         *     * Neither the name of F(X)yz, any associated website, nor the
+         * names of its contributors may be used to endorse or promote products
+         * derived from this software without specific prior written permission.
+         *
+         * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+         * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+         * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+         * DISCLAIMED. IN NO EVENT SHALL F(X)yz BE LIABLE FOR ANY
+         * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+         * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+         * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+         * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+         * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+         * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+         */
+        // Source: https://github.com/FXyz/FXyz/blob/master/FXyz-Samples/src/main/java/org/fxyz3d/samples/utilities/FloatingLabels.java
+
+        if (!render) {
+            // Hide labels
+            for (Target target : Target.allTargets) {
+                this.label.setVisible(false);
+            }
+            // We are done, end method.
+            return;
+        }
+
         for (Target target : Target.allTargets) {
             Point3D coords3D = target.localToScene(Point3D.ZERO, true);
-            System.out.println("3d target Coordinates: " + coords3D.toString());
 
             //Clipping Logic
             //if coordinates are outside of the scene it could
@@ -71,26 +114,50 @@ public class Target extends Sphere {
                 x = 0;
             }
 
-            Label label = new Label(" " + this.id);
 
             //is it right of the view?
-            if((x+label.getWidth()+5) > map3D.getWidth()) {
-                x = map3D.getWidth() - (label.getWidth()+5);
+            if(x + this.label.getWidth() + 5 > map3D.getWidth()) {
+                x = map3D.getWidth() - (this.label.getWidth() + 5);
             }
             //is it above the view?
             if(y < 0) {
                 y = 0;
             }
             //is it below the view
-            if((y+label.getHeight()) > map3D.getHeight())
-                y = map3D.getHeight() - (label.getHeight()+5);
+            if(y + this.label.getHeight() > map3D.getHeight()){
+                y = map3D.getHeight() - (this.label.getHeight() + 5);
+            }
             //@DEBUG SMP  useful debugging print
             //System.out.println("clipping Coordinates: " + x + ", " + y);
             //update the local transform of the label.
-            label.getTransforms().setAll(new Translate(x, y));
-            labelPane.getChildren().add(label);
-        }
+            this.label.getTransforms().setAll(new Translate(x, y));
 
+            // Make label visible in case it was made invisible when all labels were un-rendered.
+            this.label.setVisible(true);
+
+            // Add labels to scene if they don't exist. If they exist, change their position.
+            boolean labelExists = false;
+            for (Node node : labelPane.getChildren()) {
+                // Not all nodes in labelPane are guaranteed to be Labels.
+                if (node instanceof Label) {
+
+                    Label currentLabel = (Label) node;
+
+                    // This node is a Label, but is it this target's Label?
+                    if (currentLabel == this.label) {
+
+                        // Label has already been added to scene. Position has already been changed,
+                        // so there's nothing left to do.
+                        labelExists = true;
+                        break;
+                    }
+                }
+            }
+            if (!labelExists) {
+                // Label has not been added to scene yet, so add it.
+                labelPane.getChildren().add(this.label);
+            }
+        }
     }
 
     @Override
